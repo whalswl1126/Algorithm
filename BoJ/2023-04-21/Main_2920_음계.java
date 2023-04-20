89060fcc065a042ec60a0899cdec06450df63c73
조민지 <dnflwhalswl@naver.com>
2023-04-21 01:29:17 +0900

    [20230421] 2920 음계

---
+++ BoJ/2023-04-21/Main_2920_음계.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2920_음계 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine(); 
+		String ascending = "1 2 3 4 5 6 7 8";
+		String descending = "8 7 6 5 4 3 2 1";
+		
+		if(input.equals(ascending))
+			System.out.println("ascending");
+		else if(input.equals(descending))
+			System.out.println("descending");
+		else
+			System.out.println("mixed");
+	}
+}
