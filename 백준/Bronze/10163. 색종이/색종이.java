43b6be49ec42d4ef8fc90c4fb70cc6fbe98b5f43
whalswl1126 <122023970+whalswl1126@users.noreply.github.com>
2023-12-10 00:57:24 +0900

    [Bronze I] Title: 색종이, Time: 280 ms, Memory: 23660 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -6,39 +6,32 @@ import java.util.StringTokenizer;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int N = Integer.parseInt(br.readLine());
 		int[][] area = new int[1001][1001];
-		int[] size = new int[N];
-		int max_x = Integer.MIN_VALUE;
-		int max_y = Integer.MIN_VALUE;
-		int min_x = Integer.MAX_VALUE;
-		int min_y = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
+		int[] cnt = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int x2 = Integer.parseInt(st.nextToken()) + x1;
 			int y2 = Integer.parseInt(st.nextToken()) + y1;
 
-			max_x = Math.max(x1, Math.max(max_x, x2));
-			max_y = Math.max(y1, Math.max(max_y, y2));
-			min_x = Math.min(x1, Math.min(min_x, x2));
-			min_y = Math.min(y1, Math.min(min_y, y2));
-
 			for (int x = x1; x < x2; x++) {
 				for (int y = y1; y < y2; y++) {
-					area[x][y] = i+1;
+					area[x][y] = i;
 				}
 			}
 		}
-		for (int x = min_x; x < max_x; x++) {
-			for (int y = min_y; y < max_y; y++) {
-				size[area[x][y]-1]++;
+		for (int x = 0; x < 1001; x++) {
+			for (int y = 0; y < 1001; y++) {
+				cnt[area[x][y]]++;
 			}
 		}
 		
-		for (int j : size) {
-			System.out.println(j);
+		for (int i = 1; i <= N; i++) {			
+			System.out.println(cnt[i]);
 		}
 	}
 }
